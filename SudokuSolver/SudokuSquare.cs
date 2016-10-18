bdf0bf10842611ec1e6277366cd0e6b3c31c5d34
Stephane Larocque <stephl001@gmail.com>
2016-10-17 22:48:18 -0400

    start implementation of SudokuSquare class
    This class represents a single square on a Sudoku puzzle.
    A square can either contain a value (fixed) or possible candidates.
    There are 81 squares on a typical 9x9 sudoku puzzle.

---
+++ SudokuSolver/SudokuSquare.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+
+namespace SudokuSolver
+{
+    public sealed class SudokuSquare : IEquatable<SudokuSquare>
+    {
+        private const int MaxRange = 9;
+        private static readonly int[] EmptyCandidates = new int[] {};
+
+        public SudokuSquare(int x, int y, int value)
+            : this(x, y, value, EmptyCandidates)
+        {            
+        }
+
+        public SudokuSquare(int x, int y, int[] candidates)
+            : this(x, y, 0, candidates)
+        {
+        }
+
+        private SudokuSquare(int x, int y, int value, int[] candidates)
+        {
+            if (x < 0 || x >= MaxRange)
+                throw new ArgumentOutOfRangeException($"{nameof(x)} must be greater or equal than 0 and lower than {MaxRange}.");
+            if (y < 0 || y >= MaxRange)
+                throw new ArgumentOutOfRangeException($"{nameof(y)} must be greater or equal than 0 and lower than {MaxRange}.");
+            if (value >= MaxRange)
+                throw new ArgumentOutOfRangeException($"{nameof(value)} must be greater or equal than 0 and lower than {MaxRange}.");
+            if (candidates == null)
+                throw new ArgumentNullException(nameof(candidates));
+            if (candidates.Length > MaxRange)
+                throw new ArgumentOutOfRangeException($"The list of candidates cannot contain more than {MaxRange} elements.");
+            if (candidates.Where(c => ((c <= 0) || (c > MaxRange))).Any())
+                throw new ArgumentOutOfRangeException($"A candidate must have a value greater than 0 and smaller or equal than {MaxRange}.");
+            if (candidates.Distinct().Count() != candidates.Length)
+                throw new ArgumentException("A list of candidates cannot duplicate values.");
+
+            X = x;
+            Y = y;
+            Value = value;
+            Candidates = candidates;
+        }
+
+        public int X { get; }
+
+        public int Y { get; }
+
+        public int Value { get; }
+
+        public bool IsValueSet { get { return Value > 0; } }
+
+        public int[] Candidates { get; }
+
+        public SudokuSquare ClearCandidates(int[] candidates)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SudokuSquare KeepCandidates(int[] candidates)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"Position: ({X}, {Y}), Value: {Value}, Candidates: {{{string.Join(", ", Candidates.Select(c => c.ToString()))}}}";
+        }
+
+        public bool Equals(SudokuSquare other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SudokuSolverTests/SudokuSquareTests.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SudokuSolver;
+using System.Linq;
+
+namespace SudokuSolverTests
+{
+    [TestClass]
+    public class SudokuSquareTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestInvalidCreationX()
+        {
+            var s = new SudokuSquare(-1, 0, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestInvalidCreationY()
+        {
+            var s = new SudokuSquare(0, -1, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestInvalidRangeCreationY()
+        {
+            var s = new SudokuSquare(0, 9, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestInvalidRangeCreationX()
+        {
+            var s = new SudokuSquare(9, 5, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestInvalidRangeCreationValue()
+        {
+            var s = new SudokuSquare(3, 5, 9);
+        }
+
+        [TestMethod]
+        public void TestValidCreationValue()
+        {
+            var s = new SudokuSquare(3, 5, 4);
+            Assert.IsTrue(s.IsValueSet);
+            Assert.AreEqual(4, s.Value);
+            Assert.IsTrue(s.Candidates.Length == 0);
+            Assert.AreEqual(3, s.X);
+            Assert.AreEqual(5, s.Y);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestInvalidCreationCandidatesX()
+        {
+            var s = new SudokuSquare(-1, 0, new int[] { 1 });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestInvalidCreationCandidatesY()
+        {
+            var s = new SudokuSquare(0, -1, new int[] { 1 });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestInvalidRangeCreationCandidatesY()
+        {
+            var s = new SudokuSquare(0, 9, new int[] { 1 });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestInvalidRangeCreationCandidatesX()
+        {
+            var s = new SudokuSquare(9, 5, new int[] { 1 });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestInvalidCreationCandidates()
+        {
+            var s = new SudokuSquare(3, 5, null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestInvalidCreationTooMuchCandidates()
+        {
+            var s = new SudokuSquare(3, 5, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestInvalidCreationCandidateValueTooSmall()
+        {
+            var s = new SudokuSquare(3, 5, new int[] { 2, 4, 0 });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestInvalidCreationCandidateValueTooBig()
+        {
+            var s = new SudokuSquare(3, 5, new int[] { 2, 4, 10 });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestInvalidCreationCandidateDuplicateValues()
+        {
+            var s = new SudokuSquare(3, 5, new int[] { 2, 4, 5, 2 });
+        }
+
+        [TestMethod]
+        public void TestValidCreationCandidates()
+        {
+            var candidates = new int[] { 6, 8, 9 };
+            var s = new SudokuSquare(3, 5, candidates);
+            Assert.IsFalse(s.IsValueSet);
+            Assert.AreEqual(0, s.Value);
+            Assert.IsTrue(s.Candidates.SequenceEqual(candidates));
+            Assert.AreEqual(3, s.X);
+            Assert.AreEqual(5, s.Y);
+        }
+
+        [TestMethod]
+        public void TestToString()
+        {
+            var s = new SudokuSquare(3, 5, 4);
+            Assert.AreEqual($"Position: (3, 5), Value: 4, Candidates: {{}}", s.ToString());
+
+            s = new SudokuSquare(3, 5, new int[] { 4, 8, 9 });
+            Assert.AreEqual($"Position: (3, 5), Value: 0, Candidates: {{4, 8, 9}}", s.ToString());
+        }
+    }
+}
