bb5a83584a5e9b4aeb20e5ea13eaee339cb60360
Stephane Larocque <stephl001@gmail.com>
2016-11-01 20:54:35 -0400

    Start implementation of SudokuStrategy

---
+++ SudokuSolver/ISudokuStrategy.cs
@@ -0,0 +1,9 @@
+﻿namespace SudokuSolver
+{
+    public interface ISudokuStrategy
+    {
+        string Name { get; }
+
+        SudokuStrategyResult Query(SudokuPuzzle puzzle);
+    }
+}
---
+++ SudokuSolver/SudokuStrategyResult.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace SudokuSolver
+{
+    public enum StrategyResultOutcome
+    {
+        NotFound,
+        ValueFound,
+        ImpossibleCandidatesFound
+    }
+
+    public sealed class SudokuStrategyResult
+    {
+        
+        public StrategyResultOutcome Result { get; }
+
+        public IEnumerable<SudokuSquare> AffectedSquares { get; }
+
+        public int? Value { get; }
+
+        public IEnumerable<int> Candidates { get; }
+    }
+}
